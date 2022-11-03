668f72a8a68cf280da900a0c3292d44efcee948a
Aniket Deshpande <aniket.deshpande@iohk.io>
2022-11-03 20:42:30 +0530

    Rename SnapShot, SnapShots and Certificates.Delegation fields without underscores

--- eras/shelley/impl/src/Cardano/Ledger/Shelley/Rules/NewEpoch.hs
+++ eras/shelley/impl/src/Cardano/Ledger/Shelley/Rules/NewEpoch.hs
@@ -183,7 +183,7 @@ newEpochTransition = do
       let adaPots = totalAdaPotsES es'''
       tellEvent $ TotalAdaPotsEvent adaPots
       let ss = esSnapshots es'''
-          pd' = calculatePoolDistr (_pstakeSet ss)
+          pd' = calculatePoolDistr (ssStakeSet ss)
       pure $
         src
           { nesEL = e,
